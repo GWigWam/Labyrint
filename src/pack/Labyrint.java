7e89db836f3c9ed130d5e9f90f82ea8116af6a7a
GWigWam <welwisam@gmail.com>
2017-11-11 19:31:30 +0100

    Do not hard-set lwjgl libpath in code

--- src/pack/Labyrint.java
+++ src/pack/Labyrint.java
@@ -2,7 +2,7 @@ package pack;
 
 public class Labyrint {
 	public static void main(String[] args) {
-		System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "\\native\\windows");
+		//System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "\\native\\windows");
 		Displayer disp = new Displayer();
 	}
 }
